212c8fbab81e2bb6cb3838454109c195f78f6c83
Georgi Georgiev <vuryss@gmail.com>
2018-12-04 16:26:56 +0200

    Day 4

--- day-4.php
+++ day-4.php
@@ -8,7 +8,7 @@ sort($input);
 
 // Parse guard data & record most sleep time per guard and most sleep time for each guard per minute.
 $guard = $startSleep = 0;
-$mostSleepTime = $minuteSleepTime = [];
+$mostSleepTime = $minuteSleepTime = $guardSleep = [];
 
 foreach ($input as $line) {
     preg_match('/\:(\d{2}).*?(?:(?<guard>\d+)|(?<wakes>wakes)|(?<falls>falls))/', $line, $matches);
@@ -18,30 +18,17 @@ foreach ($input as $line) {
 
     if (isset($matches['wakes'])) {
         for ($i = $startSleep; $i < (int) $matches[1]; $i++) {
-            $mostSleepTime[$guard]       = ($mostSleepTime[$guard] ?? 0) + 1;
-            $minuteSleepTime[$i][$guard] = ($minuteSleepTime[$i][$guard] ?? 0) + 1;
+            $mostSleepTime[$guard]         = ($mostSleepTime[$guard] ?? 0) + 1;
+            $minuteSleepTime[$guard][$i]   = ($minuteSleepTime[$guard][$i] ?? 0) + 1;
+            $guardSleep[$guard . '.' . $i] = ($guardSleep[$guard . '.' . $i] ?? 0) + 1;
         }
     }
 }
 
-// Guard with most sleep time (Part 1)
 arsort($mostSleepTime);
 $guard = key($mostSleepTime);
+arsort($minuteSleepTime[$guard]);
+arsort($guardSleep);
 
-// Find most sleeping guard most lazy day & the guard sleeping most in a given minute
-$max1 = $max2 = $min1 = $min2 = $gid = 0;
-foreach ($minuteSleepTime as $minute => $guards) {
-    isset($guards[$guard]) && $guards[$guard] > $max1
-        && ($max1 = $guards[$guard])
-        && ($min1 = $minute);
-    
-    foreach ($guards as $id => $times) {
-        $times > $max2
-            && ($max2 = $times)
-            && ($min2 = $minute)
-            && ($gid = $id);
-    }
-}
-
-echo 'Answer 1: ' . ($min1 * $guard) . PHP_EOL;
-echo 'Answer 2: ' . ($min2 * $gid) . PHP_EOL;
+echo 'Answer 1: ' . ($guard * key($minuteSleepTime[$guard])) . PHP_EOL;
+echo 'Answer 2: ' . array_product(explode('.', key($guardSleep))) . PHP_EOL;
