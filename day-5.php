24b492b4c073c576e084ec815438e1e2c02af689
Georgi Georgiev <vuryss@gmail.com>
2018-12-05 07:50:08 +0200

    Day 5

--- day-5.php
+++ day-5.php
@@ -4,7 +4,7 @@ $input = trim(file_get_contents('input/' . substr(basename(__FILE__), 0, -4)));
 
 $matches = [];
 
-for ($char = ord('a'); $char <= ord('z') && $c = chr($char); $char++) {
+for ($char = 97; $char <= 122 && $c = chr($char); $char++) {
     array_push($matches, $c . strtoupper($c), strtoupper($c) . $c);
 }
 
@@ -13,9 +13,7 @@ $results = [];
 for ($char = 96; $char <= 122; $char++) {
     $chars = str_replace([chr($char), strtoupper(chr($char))], '', $input);
 
-    do {
-        $chars = str_replace($matches, '', $chars, $cont);
-    }
+    do $chars = str_replace($matches, '', $chars, $cont);
     while ($cont);
 
     $results[$char] = strlen($chars);
