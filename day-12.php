3876fd1f4f72f9711a2ef29db9fcce21a783ecbf
Georgi Georgiev <vuryss@gmail.com>
2018-12-12 13:49:16 +0200

    Day 12

--- day-12.php
+++ day-12.php
@@ -1,45 +1,39 @@
 <?php
 
 $input = trim(file_get_contents('input/' . substr(basename(__FILE__), 0, -4)));
+
+$start = microtime(true);
+
 $input = explode("\n", $input);
 
-$state = str_split(substr($input[0], 15));
+$state      = str_split(substr($input[0], 15));
 $rulesInput = array_slice($input, 2);
-$rules = [];
+$rules      = [];
 
 foreach ($rulesInput as $line) {
     $parts = explode(' => ', $line);
-    $rules[] = [str_split($parts[0]), $parts[1]];
+    $rules[$parts[0]] = $parts[1];
 }
 
 $save = 0;
 
-for ($i = 1; $i <= 2000; $i++) {
-    ksort($state, SORT_NUMERIC);
-    $first = array_search('#', $state) - 2;
-    $last = array_search('#', array_reverse($state, true)) + 2;
-    $newState = array_slice($state, $first, $last - $first + 1, true);
+for ($i = 1; $i <= 1000; $i++) {
+    $first    = array_search('#', $state) - 2;
+    $last     = array_search('#', array_reverse($state, true)) + 2;
+    $newState = [];
 
     for ($pos = $first; $pos <= $last; $pos++) {
-        if (!isset($newState[$pos])) {
-            $newState[$pos] = '.';
-        }
-
-        foreach ($rules as $rule) {
-            $a = $rule[0];
-
-            if ($a[0] === ($state[$pos - 2] ?? '.')
-                && $a[1] === ($state[$pos -1] ?? '.')
-                && $a[2] === ($state[$pos] ?? '.')
-                && $a[3] === ($state[$pos + 1] ?? '.')
-                && $a[4] === ($state[$pos + 2] ?? '.')
+        foreach ($rules as $pattern => $result) {
+            if ($pattern[0] === ($state[$pos - 2] ?? '.')
+                && $pattern[1] === ($state[$pos -1] ?? '.')
+                && $pattern[2] === ($state[$pos] ?? '.')
+                && $pattern[3] === ($state[$pos + 1] ?? '.')
+                && $pattern[4] === ($state[$pos + 2] ?? '.')
             ) {
-                $newState[$pos] = $rule[1];
+                $newState[$pos] = $result;
                 continue 2;
             }
         }
-
-        $newState[$pos] = '.';
     }
 
     $state = $newState;
@@ -56,6 +50,14 @@ for ($i = 1; $i <= 2000; $i++) {
         echo 'Answer 1: ' . $sum . PHP_EOL;
     }
 
+    if ($i === 500) {
+        foreach ($state as $key => $value) {
+            if ($value === '#') {
+                $save += $key;
+            }
+        }
+    }
+
     if ($i === 1000) {
         $sum = 0;
 
@@ -65,20 +67,9 @@ for ($i = 1; $i <= 2000; $i++) {
             }
         }
 
-        $save = $sum;
-    }
-
-    if ($i === 2000) {
-        $sum = 0;
-
-        foreach ($state as $key => $value) {
-            if ($value === '#') {
-                $sum += $key;
-            }
-        }
-
-        $diff = $sum - $save;
-        $times = 50000000000 / 1000 - 1;
-        echo 'Answer 2: ' . ($times * $diff + $save) . PHP_EOL;
+        $times = 50000000000 / 500 - 1;
+        echo 'Answer 2: ' . ($times * ($sum - $save) + $save) . PHP_EOL;
     }
 }
+
+echo 'Execution time: ' . (microtime(true) - $start) . PHP_EOL;
