0e5bb9eacec1f1436787160f48ec140ae83b7d3e
Georgi Georgiev <vuryss@gmail.com>
2018-12-12 10:26:39 +0200

    Day 12

--- day-12.php
+++ day-12.php
@@ -1,39 +1,6 @@
 <?php
 
-$input = 'initial state: #..####.##..#.##.#..#.....##..#.###.#..###....##.##.#.#....#.##.####.#..##.###.#.......#............
-
-##... => .
-##.## => .
-.#.#. => #
-#..#. => .
-#.### => #
-.###. => .
-#.#.. => .
-##..# => .
-..... => .
-...#. => .
-.#..# => .
-####. => #
-...## => #
-..### => #
-#.#.# => #
-###.# => #
-#...# => #
-..#.# => .
-.##.. => #
-.#... => #
-.##.# => #
-.#### => .
-.#.## => .
-..##. => .
-##.#. => .
-#.##. => .
-#..## => .
-###.. => .
-....# => .
-##### => #
-#.... => .
-..#.. => #';
+$input = trim(file_get_contents('input/' . substr(basename(__FILE__), 0, -4)));
 $input = explode("\n", $input);
 
 $state = str_split(substr($input[0], 15));
@@ -45,58 +12,73 @@ foreach ($rulesInput as $line) {
     $rules[] = [str_split($parts[0]), $parts[1]];
 }
 
-$mem = [];
+$save = 0;
 
-for ($i = 1; $i <= 20; $i++) {
-    $hash = implode('', $state);
+for ($i = 1; $i <= 2000; $i++) {
+    ksort($state, SORT_NUMERIC);
+    $first = array_search('#', $state) - 2;
+    $last = array_search('#', array_reverse($state, true)) + 2;
+    $newState = array_slice($state, $first, $last - $first + 1, true);
 
-    if (isset($mem[$hash])) {
-        $newState = $mem[$hash];
-    } else {
-        ksort($state, SORT_NUMERIC);
-        $first = array_search('#', $state) - 2;
-        $last = array_search('#', array_reverse($state, true)) + 2;
-        $newState = array_slice($state, $first, $last - $first + 1, true);
-
-        for ($pos = $first; $pos <= $last; $pos++) {
-            if (!isset($newState[$pos])) {
-                $newState[$pos] = '.';
-            }
-
-            foreach ($rules as $rule) {
-                $a = $rule[0];
-
-                if ($a[0] === ($state[$pos - 2] ?? '.')
-                    && $a[1] === ($state[$pos -1] ?? '.')
-                    && $a[2] === ($state[$pos] ?? '.')
-                    && $a[3] === ($state[$pos + 1] ?? '.')
-                    && $a[4] === ($state[$pos + 2] ?? '.')
-                ) {
-                    $newState[$pos] = $rule[1];
-                    continue 2;
-                }
-            }
-
+    for ($pos = $first; $pos <= $last; $pos++) {
+        if (!isset($newState[$pos])) {
             $newState[$pos] = '.';
         }
 
-        $mem[implode('', $state)] = $newState;
+        foreach ($rules as $rule) {
+            $a = $rule[0];
+
+            if ($a[0] === ($state[$pos - 2] ?? '.')
+                && $a[1] === ($state[$pos -1] ?? '.')
+                && $a[2] === ($state[$pos] ?? '.')
+                && $a[3] === ($state[$pos + 1] ?? '.')
+                && $a[4] === ($state[$pos + 2] ?? '.')
+            ) {
+                $newState[$pos] = $rule[1];
+                continue 2;
+            }
+        }
+
+        $newState[$pos] = '.';
     }
 
     $state = $newState;
 
-    if ($i % 10000 == 0) {
-        echo $i . PHP_EOL;
+    if ($i === 20) {
+        $sum = 0;
+
+        foreach ($state as $key => $value) {
+            if ($value === '#') {
+                $sum += $key;
+            }
+        }
+
+        echo 'Answer 1: ' . $sum . PHP_EOL;
+    }
+
+    if ($i === 1000) {
+        $sum = 0;
+
+        foreach ($state as $key => $value) {
+            if ($value === '#') {
+                $sum += $key;
+            }
+        }
+
+        $save = $sum;
+    }
+
+    if ($i === 2000) {
+        $sum = 0;
+
+        foreach ($state as $key => $value) {
+            if ($value === '#') {
+                $sum += $key;
+            }
+        }
+
+        $diff = $sum - $save;
+        $times = 50000000000 / 1000 - 1;
+        echo 'Answer 2: ' . ($times * $diff + $save) . PHP_EOL;
     }
 }
-
-$sum = 0;
-
-foreach ($state as $key => $value) {
-    if ($value === '#') {
-        $sum += $key;
-    }
-}
-
-echo 'Answer 1: ' . $sum . PHP_EOL;
-
