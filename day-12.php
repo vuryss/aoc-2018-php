4785eaadc6e36ad5d42c9044879192a478ff9c2d
Georgi Georgiev <vuryss@gmail.com>
2018-12-12 09:00:12 +0200

    Day 12 - Incomplete

--- day-12.php
+++ day-12.php
@@ -45,65 +45,47 @@ foreach ($rulesInput as $line) {
     $rules[] = [str_split($parts[0]), $parts[1]];
 }
 
-//echo 'State: ' . implode('', $state) . PHP_EOL;
-
-//for ($q = -5; $q < 40; $q++) {
-//    echo $state[$q] ?? ' ';
-//}
-//echo PHP_EOL;
-
 $mem = [];
 
 for ($i = 1; $i <= 20; $i++) {
-    ksort($state, SORT_NUMERIC);
-    $first = array_search('#', $state) - 2;
-    $last = array_search('#', array_reverse($state, true)) + 2;
-    $newState = array_slice($state, $first, $last - $first + 1, true);
-    //print_r($state);
+    $hash = implode('', $state);
 
-    //$hash = implode('', $newState);
+    if (isset($mem[$hash])) {
+        $newState = $mem[$hash];
+    } else {
+        ksort($state, SORT_NUMERIC);
+        $first = array_search('#', $state) - 2;
+        $last = array_search('#', array_reverse($state, true)) + 2;
+        $newState = array_slice($state, $first, $last - $first + 1, true);
 
-    //if (isset($mem[$hash])) {
-    //    echo 'HAS IT!' . PHP_EOL;
-    //}
+        for ($pos = $first; $pos <= $last; $pos++) {
+            if (!isset($newState[$pos])) {
+                $newState[$pos] = '.';
+            }
 
-    //echo 'First: ' . $first . ' | Last: ' . $last . PHP_EOL;
+            foreach ($rules as $rule) {
+                $a = $rule[0];
 
-    for ($pos = $first; $pos <= $last; $pos++) {
-        if (!isset($newState[$pos])) {
+                if ($a[0] === ($state[$pos - 2] ?? '.')
+                    && $a[1] === ($state[$pos -1] ?? '.')
+                    && $a[2] === ($state[$pos] ?? '.')
+                    && $a[3] === ($state[$pos + 1] ?? '.')
+                    && $a[4] === ($state[$pos + 2] ?? '.')
+                ) {
+                    $newState[$pos] = $rule[1];
+                    continue 2;
+                }
+            }
+
             $newState[$pos] = '.';
         }
 
-        foreach ($rules as $rule) {
-            $a = $rule[0];
-
-            if ($a[0] === ($state[$pos - 2] ?? '.')
-                && $a[1] === ($state[$pos -1] ?? '.')
-                && $a[2] === ($state[$pos] ?? '.')
-                && $a[3] === ($state[$pos + 1] ?? '.')
-                && $a[4] === ($state[$pos + 2] ?? '.')
-            ) {
-                // Match!
-                //echo 'Matched rule: ' . implode('', $a) . ' for pos: ' . $pos . PHP_EOL;
-                $newState[$pos] = $rule[1];
-                continue 2;
-            }
-        }
-
-        $newState[$pos] = '.';
+        $mem[implode('', $state)] = $newState;
     }
 
-    //$mem[implode('', $state)] = implode('', $newState);
     $state = $newState;
-    //print_r($newState);
 
-    //for ($q = -5; $q < 200; $q++) {
-    //    echo $state[$q] ?? ' ';
-    //}
-    //echo PHP_EOL . PHP_EOL;
-
-    if ($i % 1000 == 0) {
-        //print_r($mem);
+    if ($i % 10000 == 0) {
         echo $i . PHP_EOL;
     }
 }
